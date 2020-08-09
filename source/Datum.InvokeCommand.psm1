7c5393db457cc251c1b224f824d8ece3a6bad6ae
riddlertom <tomriddler@gmail.com>
2020-08-09 16:11:01 -0700

    make a .psm1 file that works

--- source/Datum.InvokeCommand.psm1
+++ source/Datum.InvokeCommand.psm1
@@ -2,3 +2,7 @@
     This file is intentionally left empty. It is must be left here for the module
     manifest to refer to. It is recreated during the build process.
 #>
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+. $here\Public\Invoke-InvokeCommandAction.ps1
+. $here\Public\Test-InvokeCommandFilter.ps1
